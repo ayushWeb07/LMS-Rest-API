f19cc12bcdac5f79a7464ea921bafa3617f4b170
Ayush Bommana <bommanaayush07@gmail.com>
2026-08-12 01:52:51 +0530

    feat: Setup typeorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from '../config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { PostsModule } from '../posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServerConfig } from '../config/server_config.interface';
+import { DatabaseConfig } from '../config/database_config.interface';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: () => ({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'root',
+        database: 'test',
+        entities: [],
+        synchronize: true,
+      }),
     }),
     AuthModule,
     UsersModule,
