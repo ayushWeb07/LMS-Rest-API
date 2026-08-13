53ee758867cf231ee148ecf667fcb46584a52c1b
Ayush Bommana <bommanaayush07@gmail.com>
2026-08-13 21:38:02 +0530

    feat: Create the tag entity

---
+++ src/tags/tag.entity.ts
@@ -0,0 +1,61 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('tags')
+export class Tag {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    name: 'name',
+    type: 'varchar',
+    length: 25,
+    nullable: false,
+    unique: true,
+    comment: 'Name of the Tag',
+  })
+  name: string;
+
+  @Column({
+    name: 'description',
+    type: 'varchar',
+    length: 100,
+    nullable: false,
+    comment: 'Description of the Tag',
+  })
+  description: string;
+
+  @Column({
+    name: 'slug',
+    type: 'varchar',
+    length: 10,
+    nullable: false,
+    unique: true,
+    comment: 'Slug of the tag',
+  })
+  slug: string;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    comment: 'Last creation date of the tag',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    comment: 'Last updation date of the tag',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    comment: 'Last deletion date of the tag',
+  })
+  deletedAt: Date;
+}
