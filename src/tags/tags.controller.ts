5e6284f75081c654b3cfec6061677d60a1a42936
Ayush Bommana <bommanaayush07@gmail.com>
2026-08-15 00:19:25 +0530

    feat: Add create tag endpoint

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { TagsService } from './tags.service';
+import { CreateTagDto } from './dtos/create-tag.dto';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+  constructor(private readonly tagsService: TagsService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async createTag(@Body() createTagDto: CreateTagDto) {
+    const tag = await this.tagsService.createTag(createTagDto);
+
+    return {
+      message: 'Tag successfully created',
+      tag,
+    };
+  }
+}
--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Tag } from './tag.entity';
 import { Repository } from 'typeorm';
+import { CreateTagDto } from './dtos/create-tag.dto';
 
 @Injectable()
 export class TagsService {
@@ -9,4 +10,13 @@ export class TagsService {
     @InjectRepository(Tag)
     private tagsRepository: Repository<Tag>,
   ) {}
+
+  async createTag(createTagDto: CreateTagDto): Promise<Tag> {
+    // create the tag instance
+    let newTag = this.tagsRepository.create(createTagDto);
+
+    // save it
+    newTag = await this.tagsRepository.save(newTag);
+    return newTag;
+  }
 }
