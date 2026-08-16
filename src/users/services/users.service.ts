94e0314ed235961ea174f5aebc32d377182032a2
Ayush Bommana <bommanaayush07@gmail.com>
2026-08-16 22:43:28 +0530

    chore: cleanup users service

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,15 +1,8 @@
 import {
   BadRequestException,
-  forwardRef,
-  Inject,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { DatabaseConfig } from '../../config/interfaces/database_config.interface';
-import { ServerConfig } from '../../config/interfaces/server_config.interface';
-import { FindAllDto, FindOneDto } from '../users.dto';
-import { PostsService } from '../../posts/posts.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../user.entity';
 import { Repository } from 'typeorm';
@@ -27,12 +20,7 @@ import { FindUsersDto } from '../dtos/find-users.dto';
 @Injectable()
 export class UsersService {
   constructor(
-    private readonly configService: ConfigService,
-
     private readonly bulkCreateUsersService: BulkCreateUsersService,
-
-    @Inject(forwardRef(() => PostsService))
-    private readonly postsService: PostsService,
 
     @InjectRepository(User)
     private usersRepository: Repository<User>,
@@ -211,50 +199,5 @@ export class UsersService {
     await this.usersRepository.delete({
       id: deleteUserDto.id,
     });
-  }
-
-  getKeys() {
-    // create the database config
-    const databaseConfig = this.configService.get<DatabaseConfig>('database');
-
-    // create the server config
-    const serverConfig = this.configService.get<ServerConfig>('server');
-
-    return {
-      status: 'ok',
-      message: 'users - get keys',
-      databaseConfig,
-      serverConfig,
-    };
-  }
-
-  create() {
-    return {
-      status: 'ok',
-      message: 'users - create',
-    };
-  }
-
-  findAll(findAllDto: FindAllDto) {
-    return {
-      status: 'ok',
-      message: 'users - findAll',
-      findAllDto,
-    };
-  }
-
-  findOneForPost(userId: string): boolean {
-    return userId.length > 5;
-  }
-
-  findOne(findOneDto: FindOneDto) {
-    return {
-      status: 'ok',
-      message: `users - findOne - ${findOneDto.id}`,
-    };
-  }
-
-  findAllPostsOfUser() {
-    return this.postsService.findAllPostsOfUser();
   }
 }
