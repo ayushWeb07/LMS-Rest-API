56d1105d88868ea90dab4e51e5958782a2491e20
Ayush Bommana <bommanaayush07@gmail.com>
2026-08-16 22:56:34 +0530

    fix: resolve dependency injection issue in posts

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,13 +3,13 @@ import { PostsController } from './posts.controller';
 import { PostsService } from './services/posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './post.entity';
-import { MetaOption } from '../meta-options/meta-option.entity';
 import { TagsModule } from '../tags/tags.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   providers: [PostsService],
   controllers: [PostsController],
-  imports: [TypeOrmModule.forFeature([Post, MetaOption]), TagsModule],
+  imports: [TypeOrmModule.forFeature([Post]), UsersModule, TagsModule],
   exports: [PostsService],
 })
 export class PostsModule {}
