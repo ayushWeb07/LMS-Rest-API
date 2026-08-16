71dbc071e6262f22389d0d52e1d85496b6ccab8d
Ayush Bommana <bommanaayush07@gmail.com>
2026-08-16 14:25:33 +0530

    fix: use query manager inside bulk create service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -36,49 +36,51 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  async bulkCreateUsers(bulkCreateUsersDto: BulkCreateUsersDto) {
+  async bulkCreateUsers(
+    bulkCreateUsersDto: BulkCreateUsersDto,
+  ): Promise<User[]> {
     const queryRunner = this.datasource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
-    let createdUsers: User[] = [];
+    const createdUsers: User[] = [];
 
     try {
       // iterate over all the users
-      for (const user of bulkCreateUsersDto.users) {
+      for (const userDto of bulkCreateUsersDto.users) {
         // find the user by email and username
-        const existingUser = await this.usersRepository.findOne({
+        const existingUser = await queryRunner.manager.findOne(User, {
           where: [
             {
-              email: user.email,
+              email: userDto.email,
             },
             {
-              username: user.username,
+              username: userDto.username,
             },
           ],
         });
 
         if (existingUser) {
           if (
-            existingUser.email === user.email &&
-            existingUser.username === user.username
+            existingUser.email === userDto.email &&
+            existingUser.username === userDto.username
           ) {
             throw new BadRequestException(
               `User with such email and username already exists`,
             );
-          } else if (existingUser.email === user.email) {
+          } else if (existingUser.email === userDto.email) {
             throw new BadRequestException(
-              `Email '${user.email}' already in use`,
+              `Email '${userDto.email}' already in use`,
             );
           } else {
             throw new BadRequestException(
-              `Username '${user.username}' already taken`,
+              `Username '${userDto.username}' already taken`,
             );
           }
         }
 
         // create a new user and save it
-        let newUser = this.usersRepository.create(user);
-        newUser = await this.usersRepository.save(newUser);
+        let newUser = queryRunner.manager.create(User, userDto);
+        newUser = await queryRunner.manager.save(newUser);
 
         createdUsers.push(newUser);
       }
